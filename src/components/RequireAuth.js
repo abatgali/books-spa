8d578e1201a8813064a9c2820d7016d4c343c4a5
emmlpark <57735228+emmlpark@users.noreply.github.com>
2022-06-14 14:31:18 -0400

    Created login/logout/signup

---
+++ src/components/RequireAuth.js
@@ -0,0 +1,15 @@
+/*
+Name: Emma Parker
+Date: 06-14-2022
+File: RequireAuth.js
+Description: This script creates a component to protect pages
+ */
+
+import {Navigate, useLocation} from "react-router-dom";
+import {useAuth} from "../services/useAuth";
+
+const RequireAuth = ({children}) => {
+    let {isAuthed} = useAuth(); let location = useLocation(); if (!isAuthed) {
+        return <Navigate to="/signin" state={{ from: location }} replace />; }
+    return children; };
+export default RequireAuth;
