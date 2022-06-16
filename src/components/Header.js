eb8c90a9efa8f55fa4fcd5f7975e4ea59ba08d90
emmlpark <57735228+emmlpark@users.noreply.github.com>
2022-06-15 20:22:18 -0400

    Created the sign up stuff again

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,13 +9,13 @@ Description:
 //import {useAuth} from "../services/useAuth";
 import {NavLink} from "react-router-dom";
 import {Navbar, Nav, Container} from "react-bootstrap";
-
 import logo from '../pages/books-logo.png';
+import {useAuth} from "../services/useAuth";
 
 //This component creates a React-Bootstrap navbar. https://react-bootstrap.github.io/components/navbar/
 const Header = () => {
     const className = ({ isActive }) => isActive ? "nav-link active" : "nav-link";
-    //const {isAuthed, user} = useAuth();
+    const {isAuthed, user} = useAuth();
 
     return (
         <>
@@ -29,10 +29,13 @@ const Header = () => {
                         <Nav className="me-auto">
                             <Nav.Link href="/" >Home</Nav.Link>
                             <Nav.Link href="/books" className={className}>Books</Nav.Link>
+                            <div className="nav-separator">|</div> {isAuthed
+                            ? <NavLink to="/signout" className={className}>Sign out</NavLink>
+                            : <NavLink to="/signin" className={className}>Sign in/Sign up</NavLink> }
                         </Nav>
-                        <Nav>
-                            <Nav.Link href="#deets">Sign In</Nav.Link>
-                        </Nav>
+                        {isAuthed && user ? <div className="navbar-name">
+                            Welcome {user.name}!</div> : ""}
+
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
